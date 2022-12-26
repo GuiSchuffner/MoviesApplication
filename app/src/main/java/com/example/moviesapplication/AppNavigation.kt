316d139a3673abab3ab19479073fd5149e5a127d
guischuffner <guischuffner@gmail.com>
2022-12-26 14:36:40 -0300

    Initial nav configuration

--- app/src/main/java/com/example/moviesapplication/AppNavigation.kt
+++ app/src/main/java/com/example/moviesapplication/AppNavigation.kt
@@ -1,6 +1,24 @@
 package com.example.moviesapplication
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
 sealed class NavRoutes(val route: String) {
     object Home: NavRoutes("home")
-    object MoviesGenres: NavRoutes("genres")
+    object MoviesGenres: NavRoutes("movies_genres")
+}
+
+@Composable
+fun NavigationSetup(navController: NavHostController){
+    NavHost(navController = navController, startDestination = )
+}
+
+sealed class BottomNavItem(
+    val route: String,
+    val icon: Int
+) {
+    object Home: BottomNavItem(NavRoutes.Home.route, R.drawable.ic_movie_icon)
+    object MoviesGenre: BottomNavItem(NavRoutes.MoviesGenres.route, R.drawable.ic_genre_icon)
 }
