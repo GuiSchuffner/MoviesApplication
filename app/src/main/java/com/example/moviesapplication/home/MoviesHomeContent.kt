8219e5de9fa17d9a66a8b25a7a87f3445fcb984d
guischuffner <guischuffner@gmail.com>
2023-01-20 17:51:00 -0300

    improve movies list item card layout

--- app/src/main/java/com/example/moviesapplication/home/MoviesHomeContent.kt
+++ app/src/main/java/com/example/moviesapplication/home/MoviesHomeContent.kt
@@ -8,12 +8,15 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
@@ -24,6 +27,7 @@ import coil.compose.AsyncImage
 import com.example.moviesapplication.model.Movies
 import org.koin.androidx.compose.koinViewModel
 import com.example.moviesapplication.R
+import com.example.moviesapplication.model.MovieImage
 import com.example.moviesapplication.ui.theme.Navy
 import com.example.moviesapplication.ui.theme.Yellow
 
@@ -154,40 +158,46 @@ fun MoviesList(
 
 @Composable
 fun MovieCard(movie: Movies, onClick: (String)-> Unit){
-    Card(
-        modifier = Modifier
-            .size(width = 160.dp, height = 320.dp)
-            .padding(vertical = 12.dp, horizontal = 12.dp)
-            .clickable(enabled = true, onClick = { onClick(movie.id) } ),
-        elevation = CardDefaults.cardElevation(defaultElevation = 16.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Navy,
-            contentColor = Yellow
-        )
-    ) {
-        Column(
+    Column(modifier = Modifier.padding(12.dp)){
+        Card(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
+                .size(width = 160.dp, height = 300.dp)
+                .clickable(enabled = true, onClick = { onClick(movie.id) }),
+            elevation = CardDefaults.cardElevation(defaultElevation = 16.dp),
+            shape = RoundedCornerShape(20.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Navy,
+                contentColor = Yellow
+            )
         ) {
-            if(movie.image!=null){
-                AsyncImage(
-                    model = movie.image.url,
-                    contentDescription = null,
-                    modifier = Modifier.size(180.dp),
-                    alignment = Alignment.Center
-                )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+            ) {
+                if(movie.image!=null){
+                    AsyncImage(
+                        model = movie.image.url,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp),
+                        contentScale = ContentScale.FillBounds
+                    )
+                }
+                else {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_launcher_background),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxWidth()
+                            .height(200.dp),
+                        contentDescription = null,
+                        contentScale = ContentScale.FillBounds
+                    )
+                }
+                MovieCardText(movie = movie)
             }
-            else {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    modifier = Modifier.size(180.dp),
-                    contentDescription = null,
-                    alignment = Alignment.Center
-                )
-            }
-            MovieCardText(movie = movie)
         }
     }
 }
@@ -198,44 +208,56 @@ fun MovieCardPreview(){
     val movie = Movies(
         title = "Game of Thrones", id = "test", numberOfEpisodes = 70, seriesEndYear = 2011,
         seriesStartYear = 2019, titleType = "Series", year = 2011, principals = listOf(),
-        image = null
+        image = MovieImage(
+            url = "https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_.jpg",
+            id = "/title/tt0944947/images/rm4204167425",
+            width = 1102,
+            height = 1500
+        ),
     )
     MovieCard(movie = movie, onClick = {})
 }
 
 @Composable
 fun MovieCardText(movie: Movies){
-    Column(
-        modifier = Modifier.padding(start = 12.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        if(movie.title!=null) {
-            Text(
-                text = movie.title,
-                style = MaterialTheme.typography.titleSmall,
-                textAlign = TextAlign.Center
-            )
+    Column {
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(start = 12.dp, end = 12.dp, top = 4.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            if(movie.title!=null) {
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.titleSmall,
+                    textAlign = TextAlign.Center
+                )
+            }
         }
-        if (movie.seriesStartYear > 0) {
-            Text(
-                text = "Series start year: ${movie.seriesStartYear}",
-                style = MaterialTheme.typography.bodySmall,
-                textAlign = TextAlign.Justify
-            )
-        }
-        if(movie.seriesEndYear > 0 ) {
-            Text(
-                text = "Series end year: ${movie.seriesEndYear}",
-                style = MaterialTheme.typography.bodySmall,
-                textAlign = TextAlign.Justify
-            )
-        }
-        if (movie.seriesEndYear <= 0 && movie.seriesStartYear <= 0 && movie.year > 0) {
-            Text(
-                text = "Movie year: ${movie.year}",
-                style = MaterialTheme.typography.bodySmall,
-                textAlign = TextAlign.Justify
-            )
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(start = 12.dp, end = 12.dp, top = 4.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            if (movie.seriesStartYear > 0) {
+                Text(
+                    text = "Series start year: ${movie.seriesStartYear}",
+                    style = MaterialTheme.typography.bodySmall,
+                    textAlign = TextAlign.Justify
+                )
+            }
+            if(movie.seriesEndYear > 0 ) {
+                Text(
+                    text = "Series end year: ${movie.seriesEndYear}",
+                    style = MaterialTheme.typography.bodySmall,
+                    textAlign = TextAlign.Justify
+                )
+            }
+            if (movie.seriesEndYear <= 0 && movie.seriesStartYear <= 0 && movie.year > 0) {
+                Text(
+                    text = "Movie year: ${movie.year}",
+                    style = MaterialTheme.typography.bodySmall,
+                    textAlign = TextAlign.Justify
+                )
+            }
         }
     }
 }
